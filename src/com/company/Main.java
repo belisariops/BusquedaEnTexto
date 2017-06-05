b550a3ae9274ad5971685e7d3be5d4b4104f294f
Américo Igancio Ferrada Diaz <americo.ferrada@gmail.com>
2017-06-04 23:57:32 -0400

    test cleaner

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,21 @@
 package com.company;
 
+import textUtilities.textCleaner;
+
+import java.io.IOException;
+
 public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+
+        textCleaner cleaner = new textCleaner("54837-0.txt");
+        try {
+            cleaner.clean();
+        } catch(IOException e){
+            System.out.println("Error E/S: " + e);
+        }
+
+        System.out.print(cleaner.getRandom());
     }
 }
--- src/textUtilities/textCleaner.java
+++ src/textUtilities/textCleaner.java
@@ -1,52 +1,55 @@
 package textUtilities;
 
 import java.io.*;
-import java.sql.Time;
 import java.util.ArrayList;
 import java.util.Random;
 
-/**
- * Created by americo on 04-06-17.
- */
+
 public class textCleaner {
 
     private String nameResult;
+    private String randomResult;
     private BufferedReader file;
     private ArrayList<String> randomWord = null;
 
-    private long lenght;
+    private int count = 0;
 
     public textCleaner(String textname) {
         FileReader aFile;
         try {
-            nameResult = textname.split(".")[0] + "Result.txt";
+            String[] local = textname.split("\\.");
+            nameResult = local[0] + "Result.txt";
+            randomResult = local[0] + "RandomResult.txt";
             aFile = new FileReader(textname);
-            this.lenght = new File(textname).length();
             this.file = new BufferedReader(aFile);
         } catch (IOException e) {
             nameResult = "";
+            randomResult = "";
             System.out.println("Error E/S: " + e);
         }
     }
 
     public String clean() throws IOException{
         BufferedWriter result = new BufferedWriter(new FileWriter(nameResult));
+        BufferedWriter randResult = new BufferedWriter(new FileWriter(randomResult));
         String line;
         String cleanedLine;
-        ArrayList<String> randomAux = new ArrayList<String>();
-        ArrayList<String> wordsAux = new ArrayList<String>();
-        randomWord = new ArrayList<String>();
+        StringBuilder auxLine;
+        ArrayList<String> randomAux = new ArrayList<>();
+        ArrayList<String> wordsAux = new ArrayList<>();
+        randomWord = new ArrayList<>();
 
         String[] auxWords;
 
         int j = 0;
+        count = 0;
 
 
         while((line = this.file.readLine()) != null){
             cleanedLine = line.toLowerCase();
 
             String original = "àáâãäåæçèéêëìíîïðñòóôõöøùúûüýÿ";
-            String originalEl = "!\"#$%&'()*+,-./0123456789:;<=>?@[\\]^_`{|}~¡¢£¤¥¦§¨©ª«¬®¯°±²³´µ¶·¸¹º»¼½¾¿÷øƒ–—‘’‚“”„†‡•…‰€™";
+            String originalEl = "!\"#$%&'()*+,-\\./0123456789:;<=>?@[\\]^_`{|}~¡¢£¤¥¦§¨©ª«¬®¯°±²³´µ¶·¸¹º»¼½¾¿÷øƒ–—‘’‚“”„†‡•…‰€™ß„";
             // Cadena de caracteres ASCII que reemplazarán los originales.
             String ascii = "aaaaaaaceeeeiiiionoooooouuuuyy";
             String output = cleanedLine;
@@ -56,13 +59,15 @@ public class textCleaner {
                 output = output.replace(original.charAt(i), ascii.charAt(i));
 
             }
-            output = cleanedLine;
+
             for (int i=0; i<originalEl.length(); i++) {
                 // Reemplazamos los caracteres especiales.
 
                 output = output.replace(originalEl.charAt(i), (char)32);
 
             }
+
+            cleanedLine = output;
 
             auxWords = cleanedLine.split(" ");
 
@@ -84,10 +89,23 @@ public class textCleaner {
                 randomWord.add(this.getRandomWord(randomAux));
                 j = 0;
             }
+            if(randomWord.size()>32){
+                auxLine = new StringBuilder();
+                for (String str: randomWord) {
+                    if(!str.equals("")){
+                        auxLine.append(";");
+                        auxLine.append(str);
+                    }
+                }
+                randResult.write(auxLine.toString());
+                count++;
+                randomWord = new ArrayList<>();
+            }
         }
 
         randomWord.add(this.getRandomWord(randomAux));
 
+        randResult.close();
         result.close();
         file.close();
         return nameResult;
@@ -98,5 +116,48 @@ public class textCleaner {
         int whose = rand.nextInt(words.size());
 
         return words.get(whose);
+    }
+
+    public String getRandom(){
+        Random rand = new Random();
+        int whose;
+        if(randomWord.size() == 0){
+            whose = rand.nextInt(count);
+        }else{
+            whose = rand.nextInt(count+1);
+        }
+        if(count == 0){
+            if(randomWord.size() == 0){
+                return "";
+            }else{
+                return this.getRandomWord(randomWord);
+            }
+        }else{
+            if(whose == count+1){
+                return this.getRandomWord(randomWord);
+            }else{
+                try {
+                    FileReader auxFile = new FileReader(randomResult);
+                    BufferedReader reader = new BufferedReader(auxFile);
+                    String line;
+                    int k = 0;
+                    while ((line = reader.readLine()) != null) {
+                        if (k == whose) {
+                            ArrayList<String> val = new ArrayList<>();
+                            for (String pal : line.split(":")) {
+                                val.add(pal);
+                            }
+                            return this.getRandomWord(val);
+                        } else {
+                            k++;
+                        }
+                    }
+                }
+                catch(IOException e){
+                    System.out.println("Error E/S: " + e);
+                }
+            }
+        }
+        return "";
     }
 }
