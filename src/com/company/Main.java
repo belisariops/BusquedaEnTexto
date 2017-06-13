5efbb5c26f6e4f1e0f24c829a83da99401ceeda1
Américo Igancio Ferrada Diaz <americo.ferrada@gmail.com>
2017-06-12 22:39:49 -0400

    Ready para los test

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,7 @@ package com.company;
 import java.io.*;
 
 import algorithms.Automata;
+import algorithms.SuffixArray;
 import textUtilities.TextCleaner;
 
 import java.util.ArrayList;
@@ -12,72 +13,140 @@ public class Main {
 
     public static void main(String[] args) {
 
+
+        /*Se inicializan las variables para la ejecusion de los experimentos, BrufferedWriter son los distintos archivos para los resultados de los
+        * experimentos, list es la lista de intersecciones, tetPAth es el archivo utilizado en el momento*/
+
         String textPath;
         Automata automata;
         List<Integer> list;
         BufferedWriter resultExperiment;
+        BufferedWriter resultExperimentArray;
         BufferedWriter resultIntersecion;
 
         ArrayList<String> listaArchivos = new ArrayList<>();
-        listaArchivos.add("english50MB.txt");
-        listaArchivos.add("english100MB.txt");
-        listaArchivos.add("english200MB.txt");
-        listaArchivos.add("english1024MB.txt");
-        listaArchivos.add("english.txt");
+        listaArchivos.add("128m.txt");
+        listaArchivos.add("256m.txt");
 
         TextCleaner cleaner;
         try {
+
+            // Para cada archivo a analizar
             for(String archivo: listaArchivos) {
+
+                //Se limpia, donde se extraen los caracteres que no se consideraron, y se obtienen las palabras aleatorias del texto
                 cleaner = new TextCleaner(archivo);
 
                 textPath = cleaner.clean();
 
+                // Se crean los sitintos archivos de resultado.
+
                 resultExperiment = new BufferedWriter(new FileWriter("Experiment" + textPath));
+                resultExperimentArray = new BufferedWriter(new FileWriter("ExperimentArray" + textPath));
                 resultIntersecion = new BufferedWriter(new FileWriter("Intersecion" + textPath));
+
+                // Se obtiene la lista de palabras aleatorias
 
                 ArrayList<String> words = cleaner.getNextRandom();
 
                 BufferedReader br;
 
                 StringBuilder builder;
+                StringBuilder builderArray;
+
+                // Tiempo de contruccion del SuffixArray.
+
+                long firstTime;
+
+                // Variable apra calcular el tiempo desde donde se calcula el actual.
 
                 long fromTime;
+
+
+                // Se crea un StringBuilder como parametro para el SuffixArray
+                br = new BufferedReader(new FileReader(textPath));
+                StringBuilder text = new StringBuilder();
+                String line;
+                while((line = br.readLine())!=null){
+                    text.append(line);
+                }
+                br.close();
+
+                // Se genera el SuffixArray para el texto
+                fromTime = System.nanoTime();
+
+                SuffixArray array = new SuffixArray(text);
+
+                firstTime = System.nanoTime()-fromTime;
+
                 int j = 0;
 
+                // Para cada una de las palabras aleatorias
+
                 for (String word : words) {
-
-                    builder = new StringBuilder();
-
-                    builder.append(j);
-                    builder.append(",");
-
-                    br = new BufferedReader(new FileReader(textPath));
-
-                    fromTime = System.nanoTime();
-
-                    automata = new Automata(word);
-
-                    builder.append(fromTime - System.nanoTime());
-                    builder.append(",");
-                    fromTime = System.nanoTime();
-
-                    list = automata.run(br);
-
-                    builder.append(fromTime - System.nanoTime());
-                    builder.append(System.lineSeparator());
-
-                    resultExperiment.write(builder.toString());
-
-                    resultIntersecion.write("Repeticiones: " + automata.getCantRepeticiones() + System.lineSeparator());
-                    resultIntersecion.write("Posiciones :"+ System.lineSeparator());
-                    for (int i : list) {
-                        resultIntersecion.write(i+System.lineSeparator());
+                    if (j == 125){
+                        break;
                     }
-                    br.close();
+
+                    if(!word.equals("")) {
+
+                        builderArray = new StringBuilder();
+
+                        builder = new StringBuilder();
+
+                        builderArray.append(j);
+                        builder.append(j++);
+                        builder.append(",");
+                        builderArray.append(",");
+
+                        builderArray.append(firstTime);
+                        builderArray.append(",");
+
+                        // Se busca utilizando el array
+                        fromTime = System.nanoTime();
+
+                        array.findPattern(word);
+
+                        builderArray.append(System.nanoTime() - fromTime);
+                        builderArray.append(System.lineSeparator());
+
+                        resultExperimentArray.write(builderArray.toString());
+
+                        br = new BufferedReader(new FileReader(textPath));
+
+                        // Se crea un automata para la palabra.
+                        fromTime = System.nanoTime();
+
+                        automata = new Automata(word);
+
+                        builder.append(System.nanoTime() - fromTime);
+                        builder.append(",");
+
+                        // Se utiliza el automata sobre el texto.
+                        fromTime = System.nanoTime();
+
+                        list = automata.run(br);
+
+                        builder.append(System.nanoTime() - fromTime);
+                        builder.append(System.lineSeparator());
+
+                        resultExperiment.write(builder.toString());
+
+                        // Se escriben los resultados en el archivo de intersecciones
+                        resultIntersecion.write("Repeticiones: " + automata.getCantRepeticiones() + System.lineSeparator());
+                        resultIntersecion.write("Posiciones :" + System.lineSeparator());
+                        for (int i : list) {
+                            resultIntersecion.write(i + System.lineSeparator());
+                        }
+                        br.close();
+                    }
                 }
 
                 resultExperiment.close();
+                resultExperimentArray.close();
                 resultIntersecion.close();
+
+
 
             }
         } catch(IOException e){
--- src/textUtilities/TextCleaner.java
+++ src/textUtilities/TextCleaner.java
@@ -4,38 +4,40 @@ import java.io.*;
 import java.util.ArrayList;
 import java.util.Random;
 
-
+/*Esta clase implementa funcionalidades para limpiar y extraer las palabras random del texto dado un nombre de archivo*/
 public class TextCleaner {
 
     private String nameResult;
-    private String randomResult;
     private BufferedReader file;
     private ArrayList<String> randomWord = null;
 
+
+    /*El contuctor toma un nombre de archivo y crea los archivos de salida agregando la terminacion Result al nombre original*/
     public TextCleaner(String textname) {
         FileReader aFile;
         try {
             String[] local = textname.split("\\.");
             nameResult = local[0] + "Result.txt";
-            randomResult = local[0] + "RandomResult.txt";
             aFile = new FileReader(textname);
             this.file = new BufferedReader(aFile);
         } catch (IOException e) {
             nameResult = "";
-            randomResult = "";
             System.out.println("Error E/S: " + e);
         }
     }
 
+    /*Llamado a clean ejecuta la limpieza del texto, eliminando acentos y caracteres no esperados, a su vez obtiene las palabras aleatorias
+antes de eliminar los espacios seleccionando una de cada 10 de estas, retorna el nombre de archivo donde se coloco el resultado. */
     public String clean() throws IOException{
         BufferedWriter result = new BufferedWriter(new FileWriter(nameResult));
-        BufferedWriter randResult = new BufferedWriter(new FileWriter(randomResult));
         String line;
         String cleanedLine;
         ArrayList<String> wordsAux = new ArrayList<>();
         randomWord = new ArrayList<>();
 
         String[] auxWords;
+
+        // Para cada una de las lineas del texto
 
         while((line = this.file.readLine()) != null){
             cleanedLine = line.toLowerCase();
@@ -63,7 +65,11 @@ public class TextCleaner {
 
             cleanedLine = output;
 
+            //separo la linea ya limpiada en cada espacio
+
             auxWords = cleanedLine.split(" ");
+
+            //cada palabra la agrego a una lista, cuado esta llega a 10 saco un elemento aleatorio y vacio la lista.
 
             for(String word : auxWords){
                 wordsAux.add(word);
@@ -80,12 +86,13 @@ public class TextCleaner {
         }
 
 
-        randResult.close();
         result.close();
         file.close();
         return nameResult;
     }
 
+
+    //Selecciona un String de una lista de forma aleatoria.
     private String getRandomWord(ArrayList<String> words){
         Random rand = new Random();
         if(words.size() == 0){
@@ -96,6 +103,7 @@ public class TextCleaner {
         return words.get(whose);
     }
 
+    // Devuelve la lista de valores random.
     public ArrayList<String> getNextRandom(){
         return randomWord;
     }
